71d4e7c0d8fe458e1ba28d36751c0ad278d8ba31
Igor Patro <i.patro@wp.pl>
2023-01-22 19:31:56 +0100

    Added database lib

---
+++ apps/api/src/app.controller.ts
@@ -0,0 +1,55 @@
+import { Controller, Get } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
+
+@Controller()
+export class AppController {
+  constructor(private prisma: PrismaService) {}
+
+  // @Public()
+  @Get()
+  welcome() {
+    return `
+      <html>
+        <head>
+          <title>ART API</title>
+          <style>
+            body {
+              color: white;
+              background-color: black;
+            }
+          </style>
+        </head>
+        <body>
+          <h1>ART API Server</h1>
+          <h2>Server is ONLINE</h2>
+          <h5>PS: We love Black ❤</h5>
+        </body>
+      </html>
+    `;
+  }
+
+  // @Public()
+  // @Validate(RegisterSchema)
+  // @Post('test')
+  // async testPost(@Body() body: RegisterRequest) {
+  //   const user = await this.prisma.user.findFirst({
+  //     where: { email: body.email },
+  //   });
+
+  //   if (!user) {
+  //     throw new HttpException(messages.NOT_FOUND, HttpStatus.NOT_FOUND);
+  //   }
+
+  //   return user;
+  // }
+
+  // @Public()
+  @Get('users-test')
+  async testGet() {
+    const users = await this.prisma.user.findMany();
+
+    // throw new HttpException('TEST', 909);
+
+    return users;
+  }
+}
---
+++ apps/api/src/app.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { PrismaService } from './prisma.service';
+// import { AuthModule } from 'auth/auth.module';
+// import { UsersModule } from 'users/users.module';
+// import { JwtAuthGuard } from 'auth/jwt-auth.guard';
+// import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    // ConfigModule.forRoot({
+    //   isGlobal: true,
+    // }),
+    // AuthModule,
+    // UsersModule,
+  ],
+  controllers: [AppController],
+  providers: [
+    PrismaService,
+    // {
+    //   provide: 'APP_GUARD',
+    //   useClass: JwtAuthGuard,
+    // },
+  ],
+})
+export class AppModule {}
