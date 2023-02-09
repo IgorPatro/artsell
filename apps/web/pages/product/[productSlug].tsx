87ea06e4c3cbf402bbc8d944a90034d69f80e378
Igor Patro <i.patro@wp.pl>
2023-02-09 18:55:50 +0100

    Added data generating

--- apps/web/pages/product/[productSlug].tsx
+++ apps/web/pages/product/[productSlug].tsx
@@ -1,4 +1,6 @@
 import network, { Product } from "@artsell/network"
+import ReactMarkdown from "react-markdown"
+import Image from "next/image"
 
 interface Props {
   data: Product
@@ -9,8 +11,14 @@ const ProductPage = ({ data }: Props) => {
     <div>
       <h1>ProductPage</h1>
       <h3>
-        {data.name}, {data.price}
+        {data.name}, {data.price} zł
       </h3>
+      <Image src={data.image} alt={data.name} width={400} height={400} />
+      <p>{data.description}</p>
+      <p>Created at: {new Date(data.createdAt).toDateString()}</p>
+      <p>Updated at at: {new Date(data.updatedAt).toDateString()}</p>
+      <br />
+      <ReactMarkdown>{data.content}</ReactMarkdown>
     </div>
   )
 }
@@ -19,7 +27,6 @@ export default ProductPage
 
 export const getServerSideProps = async ({ query }) => {
   const { productSlug } = query
-
   const data = await network.get<Product>(`/products/${productSlug}`)
 
   return {
