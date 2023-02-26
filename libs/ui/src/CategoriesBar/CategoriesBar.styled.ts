6740bf23bd3f9a24beb82771b4f0671ec3e33843
Jakub Karaś <jakub.karas@onet.pl>
2023-02-26 23:33:55 +0100

    WIP: added basic bar structure, and names of list items

--- libs/ui/src/CategoriesBar/CategoriesBar.styled.ts
+++ libs/ui/src/CategoriesBar/CategoriesBar.styled.ts
@@ -1,1 +1,24 @@
 import styled from "styled-components";
+import Link from "next/link";
+
+export const CategoriesBarWrapper = styled.section`
+    width: 100%;
+    padding: 2rem 0;
+    border-top:  dotted ${({theme}) => theme.palette.black};
+    border-bottom:  dotted ${({theme}) => theme.palette.black};
+`
+
+export const ItemsList = styled.ul`
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    list-style: none;
+`
+
+export const ListElement = styled.li`
+
+`
+
+export const ListLink = styled(Link)`
+    text-decoration: none;
+`
--- libs/ui/src/CategoriesBar/CategoriesBar.tsx
+++ libs/ui/src/CategoriesBar/CategoriesBar.tsx
@@ -1,3 +1,58 @@
+import * as S from "./CategoriesBar.styled"
+
+const CategoriesBarData = [
+  {
+    key: 1,
+    title: "Dla niej",
+    path: "/",
+  },
+  {
+    key: 2,
+    title: "Dla niego",
+    path: "/",
+  },
+  {
+    key: 3,
+    title: "Dziecko",
+    path: "/",
+  },
+  {
+    key: 4,
+    title: "Akcesoria",
+    path: "/",
+  },
+  {
+    key: 5,
+    title: "Dom & mieszkanie",
+    path: "/",
+  },
+  {
+    key: 6,
+    title: "Sztuka",
+    path: "/",
+  },
+  {
+    key: 7,
+    title: "Fotografia",
+    path: "/",
+  },
+  {
+    key: 8,
+    title: "Specjalne",
+    path: "/",
+  },
+]
+
 export const CategoriesBar = () => {
-  return <div>CategoriesBar</div>
+  return (
+    <S.CategoriesBarWrapper>
+      <S.ItemsList>
+        {CategoriesBarData.map((item) => (
+          <S.ListElement key={item.key}>
+            <S.ListLink href={item.path}>{item.title}</S.ListLink>
+          </S.ListElement>
+        ))}
+      </S.ItemsList>
+    </S.CategoriesBarWrapper>
+  )
 }
